8753b8fb0644914e8bdef19305fd34eb0dcca598
Konstantin <91050739+NurulinM@users.noreply.github.com>
2022-05-01 08:51:07 +0300

    Whith bugs

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -1,9 +1,67 @@
 #include<iostream>
+#include<iomanip>
 using namespace std;
+int col; //Переменая в которую помещаеться количество операций 
+string more; //
+int const numbers = 3; //Константа для массива num
+int num [numbers]; //Массив num, сюда записываються все числа в операции
+string op;
+int two()
+
+{
+    cout << "Enter example ";
+    cin>> num[0] >> op >>num[1];
+//-----------------------------------------
+    if(op=="-");
+        {
+            cout << num[0] - num[1] <<endl;
+        }
+//-----------------------------------------
+         if(op=="+");
+        {
+            cout << num[0] + num[1] <<endl;
+        }
+//-----------------------------------------
+         if(op=="*");
+        {
+            cout << num[0] * num[1] <<endl;
+        }
+ //-----------------------------------------
+         if(op=="/");
+        {
+            cout << num[0] / num[1] <<endl;
+        }
+//------------------------------------------
+    return 0;
+}
+
+int three()
+    {
+        
+        return 0;
+    }
 
 int main(){
-    
-    cout << "hello" << endl;
+    cout << setw(45)<< "Calculator" << endl;
+        do
+            {
+                cout << setw(5) << "Enter numbers of operations (Min 2 , Max 2) ";
+                cin >> col;
+                switch (col)
+                {
+
+                case 2: 
+                    two();
+                    break;
+                
+                case 3:
+
+                    break;  
+                }  
+                
+                cout << setw(5) << "more?"<< setw(10) <<"(yes/no)" << endl; 
+                cin>> more;
+        } while (more == "yes");
+    cin.get();
     return 0;
-    system("pause");
 }
